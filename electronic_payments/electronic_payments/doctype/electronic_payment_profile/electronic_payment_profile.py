3f02b2c41cbda1479772428f6029a649f10763ae
Tyler Matteson <tyler@agritheory.com>
2023-04-25 15:53:39 -0400

    chore: fix flake8 errors

--- electronic_payments/electronic_payments/doctype/electronic_payment_profile/electronic_payment_profile.py
+++ electronic_payments/electronic_payments/doctype/electronic_payment_profile/electronic_payment_profile.py
@@ -1,16 +1,16 @@
-# -*- coding: utf-8 -*-
 # Copyright (c) 2022, AgriTheory and contributors
 # For license information, please see license.txt
 
-from __future__ import unicode_literals
+
 # import frappe
 from frappe.model.document import Document
+
 
 class ElectronicPaymentProfile(Document):
 	def validate(self):
 		if not len(self.reference) == 4:
 			return
-		if self.payment_type == 'Card':
+		if self.payment_type == "Card":
 			self.reference = f"**** **** **** {self.reference}"
-		if self.payment_type == 'ACH':
+		if self.payment_type == "ACH":
 			self.reference = f"*{self.reference}"
--- electronic_payments/electronic_payments/doctype/electronic_payment_settings/electronic_payment_settings.py
+++ electronic_payments/electronic_payments/doctype/electronic_payment_settings/electronic_payment_settings.py
@@ -1,16 +1,15 @@
-from __future__ import unicode_literals
-
 import frappe
 import json
-import uuid
-from decimal import *
-import datetime
 
 from authorizenet import apicontractsv1
-from authorizenet.apicontrollers import *
+from authorizenet.apicontrollers import getTransactionListController
 
-from electronic_payments.electronic_payments.doctype.electronic_payment_settings.authorize import AuthorizeNet
-from electronic_payments.electronic_payments.doctype.electronic_payment_settings.stripe import Stripe
+from electronic_payments.electronic_payments.doctype.electronic_payment_settings.authorize import (
+	AuthorizeNet,
+)
+from electronic_payments.electronic_payments.doctype.electronic_payment_settings.stripe import (
+	Stripe,
+)
 
 from frappe.utils.data import today
 from frappe.model.document import Document
@@ -18,14 +17,13 @@ from frappe.model.document import Document
 
 class ElectronicPaymentSettings(Document):
 	def validate(self):
-		# create mode of payment if one is not selected 
+		# create mode of payment if one is not selected
 		pass
 
-
 	def client(self):
-		if self.provider == 'Authorize.net':
+		if self.provider == "Authorize.net":
 			return AuthorizeNet()
-		if self.provider == 'Stripe':
+		if self.provider == "Stripe":
 			return Stripe()
 
 
@@ -33,9 +31,9 @@ class ElectronicPaymentSettings(Document):
 def process(doc, data):
 	doc = frappe._dict(json.loads(doc)) if isinstance(doc, str) else doc
 	data = frappe._dict(json.loads(data)) if isinstance(data, str) else data
-	settings = frappe.get_doc('Electronic Payment Settings', {'company': doc.company})
+	settings = frappe.get_doc("Electronic Payment Settings", {"company": doc.company})
 	if not settings:
-		frappe.msgprint(_(f'No Electronic Payment Settings found for {doc.company}'))
+		frappe.msgprint(frappe._(f"No Electronic Payment Settings found for {doc.company}"))
 	client = settings.client()
 	response = client.process_transaction(doc, data)
 	print(response)
@@ -44,8 +42,8 @@ def process(doc, data):
 
 @frappe.whitelist()
 def fetch_transactions():
-	for settings in frappe.get_all('Electronic Payments Settings'):
-		settings = frappe.get_doc('Electronic Payments Settings', settings)
+	for settings in frappe.get_all("Electronic Payments Settings"):
+		settings = frappe.get_doc("Electronic Payments Settings", settings)
 		sorting = apicontractsv1.TransactionListSorting()
 		sorting.orderBy = apicontractsv1.TransactionListOrderFieldEnum.id
 		sorting.orderDescending = True
@@ -55,7 +53,7 @@ def fetch_transactions():
 
 		transactionListRequest = apicontractsv1.getTransactionListRequest()
 		transactionListRequest.merchantAuthentication = settings.merchant_auth()
-		abbr = frappe.get_value('Company', settings.company, 'abbr')
+		abbr = frappe.get_value("Company", settings.company, "abbr")
 		transactionListRequest.refId = f"{today()} {abbr}"
 		transactionListRequest.sorting = sorting
 		transactionListRequest.paging = paging
@@ -66,19 +64,22 @@ def fetch_transactions():
 
 		if response is not None:
 			if response.messages.resultCode == apicontractsv1.messageTypeEnum.Ok:
-				if hasattr(response, 'transactions'):
+				if hasattr(response, "transactions"):
 					process_transactions(settings, response)
 
 
 def process_transactions(settings, response):
-	je = frappe.new_doc('Journal Entry')
+	je = frappe.new_doc("Journal Entry")
 	for entry in response.transactions:
 		# handle voided transaction
-		je.append('accounts', {
-			'account': '',
-			'party_type': '',
-			'party': '',
-			'clearance_date': batch.settlementTimeLocal,  # TODO: import batch
-			'amount': entry.statistics.statistic.chargeAmount
-		})
+		je.append(
+			"accounts",
+			{
+				"account": "",
+				"party_type": "",
+				"party": "",
+				# "clearance_date": batch.settlementTimeLocal,  # TODO: import batch
+				"amount": entry.statistics.statistic.chargeAmount,
+			},
+		)
 	return response
