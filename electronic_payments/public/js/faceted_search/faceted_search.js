d68eb7a3cd11dc692ceaa7281a5dd754686b6458
Tyler Matteson <tyler@agritheory.com>
2023-11-13 13:23:13 -0500

    wip: mount component in both list view and cart

--- electronic_payments/public/js/faceted_search/faceted_search.js
+++ electronic_payments/public/js/faceted_search/faceted_search.js
@@ -3,7 +3,7 @@ import FacetedSearch from './FacetedSearch.vue'
 frappe.provide('faceted_search')
 
 faceted_search.mount = () => {
-	if (['Item', 'Website Item', 'BOM'].includes(cur_list.doctype)) {
+	if (cur_list && ['Item', 'Website Item', 'BOM'].includes(cur_list.doctype)) {
 		if (faceted_search.$search == undefined && !$('#faceted-search').length) {
 			$('.filter-section').prepend('<li id="faceted-search"></li>')
 			setTimeout(() => {
@@ -13,8 +13,9 @@ faceted_search.mount = () => {
 				})
 			}, 100)
 		}
+	} else {
+		console.log('not Item list')
 	}
-	// if shopping cart
 }
 
 function waitForElement(selector) {
@@ -42,3 +43,10 @@ waitForElement('[data-route]').then(element => {
 	const config = { attributes: true, childList: false, characterData: true }
 	observer.observe(element, config)
 })
+
+if (document.querySelector('#product-filters')) {
+	faceted_search.$search = new window.Vue({
+		el: '#product-filters',
+		render: h => h(FacetedSearch, { props: {} }),
+	})
+}
