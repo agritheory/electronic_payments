749d6af14704ed6d42f7ab05e9be6e67b023e39e
Heather Kusmierz <heather.kusmierz@gmail.com>
2024-03-29 13:13:21 -0400

    feat: show button only when payments enabled

--- electronic_payments/public/js/purchase_invoice_custom.js
+++ electronic_payments/public/js/purchase_invoice_custom.js
@@ -2,9 +2,13 @@ frappe.provide('electronic_payments')
 
 frappe.ui.form.on('Purchase Invoice', {
 	refresh: frm => {
-		if (!frm.is_new() && !frm.is_dirty()) {
-			frm.add_custom_button(__('Electronic Payments'), () => {
-				electronic_payments.electronic_payments(frm)
+		if (!frm.is_new() && !frm.is_dirty() && frm.company) {
+			frappe.db.get_value('Electronic Payment Settings', { company: frm.company }, 'enable_sending').then(r => {
+				if (r && r.message && r.message.enable_sending) {
+					frm.add_custom_button(__('Electronic Payments'), () => {
+						electronic_payments.electronic_payments(frm)
+					})
+				}
 			})
 		}
 	},
