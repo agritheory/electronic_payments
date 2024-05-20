bce8b1ffb8d5085683753a123720bf2b102aba53
Heather Kusmierz <heather.kusmierz@gmail.com>
2024-05-20 15:17:17 -0400

    ci: update for version-15 and tests

--- .github/helper/install_dependencies.sh
+++ .github/helper/install_dependencies.sh
@@ -7,10 +7,4 @@ if grep -lr --exclude-dir=node_modules "^<<<<<<< " "${{GITHUB_WORKSPACE}}"
     exit 1
 fi
 
-sudo apt update && sudo apt install redis-server libcups2-dev
-
- # install wkhtmltopdf
-wget -O /tmp/wkhtmltox.tar.xz https://github.com/frappe/wkhtmltopdf/raw/master/wkhtmltox-0.12.3_linux-generic-amd64.tar.xz
-tar -xf /tmp/wkhtmltox.tar.xz -C /tmp
-sudo mv /tmp/wkhtmltox/bin/wkhtmltopdf /usr/local/bin/wkhtmltopdf
-sudo chmod o+x /usr/local/bin/wkhtmltopdf
+sudo apt update -y && sudo apt install redis-server libcups2-dev mariadb-client-10.6 -y
