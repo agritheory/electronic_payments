187c5b0a0faf9d2cbf75c1d709c330e36548a5c7
Heather Kusmierz <heather.kusmierz@gmail.com>
2025-04-10 18:51:31 -0400

    ci: update to poetry 2.0, pre-commit, test_utils, linting (#41)
    * ci: update to poetry 2.0, pre-commit, test_utils, linting
    
    * ci: update mariadb-client

--- .github/helper/install_dependencies.sh
+++ .github/helper/install_dependencies.sh
@@ -7,4 +7,4 @@ if grep -lr --exclude-dir=node_modules "^<<<<<<< " "${{GITHUB_WORKSPACE}}"
     exit 1
 fi
 
-sudo apt update -y && sudo apt install redis-server libcups2-dev mariadb-client-10.6 -y
+sudo apt update -y && sudo apt install redis-server libcups2-dev mariadb-client
